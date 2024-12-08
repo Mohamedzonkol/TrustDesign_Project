2ed2f77f060b51fdae30e4056f807997492c92ec
Mohamedzonol <mo.zonkol@gmail.com>
2024-12-09 01:51:43 +0200

    fix bug

--- TrustDesign_Core/Interfaces/Reporesitories/IReporostory.cs
+++ TrustDesign_Core/Interfaces/Reporesitories/IReporostory.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
 using TrustDesign_Core.Interfaces.Wrappers.Parametars;
 
 namespace TrustDesign_Core.Interfaces.Reporesitories
 {
-    public interface IReporostory<T> : IDisposable where T : class
+    public interface IReporostory<T> : IDisposable where T : class, new()
     {
-        Task<List<T>> GetAll();
+        Task<T> GetAll(Expression<Func<T, bool>> expression = null);
         Task<T> GetById(Guid id);
         Task<T> Add(T entity);
         Task<T> Update(Guid id, T entity);
-        Task<T> Delete(Guid id);
+        Task Delete(Guid id);
         Task<IQueryable<T>> GetPagedAsync(PagedRequest request, params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includeprop);
     }
 }
